3831a0862d4af1de658ba3f6736eab50a4e9a6ac
Shreeram <shreeramsaran28@gmail.com>
2025-07-16 15:02:17 +0530

    added update patient api

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -7,20 +7,20 @@ public class PatientResponseDTO {
     private String address;
     private String dateOfBirth;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
